8b4d89a5b7ece06b2c9640fb7dd969da6e58c02e
Jayson Guevarra <jzon_03@yahoo.com>
2026-06-11 15:19:04 -0400

    update

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -40,20 +40,6 @@ export class Home {
     }
   ];
 
-  schedule = {
-    friday: [
-      { time: '11:00 AM', event: 'Festival Opening Ceremony' },
-      { time: '11:30 AM', event: 'Food Vendors Open' },
-      { time: '12:00 PM', event: 'Traditional Tinikling Dance' },
-      { time: '1:00 PM', event: 'Filipino Cooking Demonstration' },
-      { time: '2:00 PM', event: 'Cultural Fashion Show' },
-      { time: '3:00 PM', event: 'Live Band Performance' },
-      { time: '4:00 PM', event: 'Kids Cultural Activities' },
-      { time: '5:00 PM', event: 'Martial Arts Demonstration' },
-      { time: '6:00 PM', event: 'Festival Closing' }
-    ]
-  };
-
   foods = [
     'Lumpia (Spring Rolls)',
     'Pancit (Noodles)',
